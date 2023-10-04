6cbfbe5a3c36ba8a692a348fda3ea6a35e17424a
Jaweria Memon <144684228+Jaweriaamemon23@users.noreply.github.com>
2023-10-04 15:31:14 +0500

    3rd commit

---
+++ Assignment3/Assets/js/index.js
@@ -0,0 +1,7 @@
+function getData() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    console.log("Username:", username.textcontent);
+    console.log("Password:", password.textcontent);
+    }
